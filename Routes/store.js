01ff0004c903cd989bdeffa927c46781db5e3883
Hesam Hosseini <hesamhosseini.dev@gmail.com>
2023-12-30 10:24:01 +0330

    Update store.js

--- Routes/store.js
+++ Routes/store.js
@@ -4,6 +4,9 @@ const route = express.Router();
 const ExecuteQuery = require("../dbConnection");
 const { ensureToken } = require("../authMethods");
 
+
+
+
 route.get("/customers", ensureToken, async (req, res) => {
   res.send(await ExecuteQuery("select * from ecommerce_db.products"));
 });
